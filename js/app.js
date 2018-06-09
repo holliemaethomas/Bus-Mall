95366cea713fde68dd62fcc6b82485d62326563c
holliemaethomas <holliemaethomas85@gmail.com>
2018-06-09 11:59:15 -0700

    adding chart

--- js/app.js
+++ js/app.js
@@ -1,91 +1,133 @@
 'use strict';
-debugger;
 
-// // ...................Global..................................
+Product.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-Product.names = ['bag', 'banana', 'bathroom','boots', 'breakfast', 'chair', 'cthulu', 'dog-duck', 'dragon','pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-product.all = [];
-product.container = document.getElementById('image_container');
-product.justViewed = [];
-product.pics = [document.getElementById('left'), document.getElementById('center'), document.getElementById('right')];
-product.tally = document.getElementById('tally');
-product.totalClicks = 0;
+//contains all the products
+Product.all = [];
+Product.container = document.getElementById('image_container');
+Product.justViewed = [];
+Product.pics = [document.getElementById('left'), document.getElementById('center'), document.getElementById('right')];
+Product.tally = document.getElementById('tally');
+Product.totalClicks = 0;
 
 
-function Product(name){
+function Product(name) {
   this.name = name;
   this.path = 'img/' + name + '.jpg';
   this.votes = 0;
   this.views = 0;
-  product.all.push(this);
-  
+  Product.all.push(this);
 }
-for(var i = 0; i< products.names; i++){
-  new Product(Product.name[i]);
-
-
-function makeRandom(){
-  return Math.floor(Math.random()* Product.names.length); 
+for (var i = 0; i < Product.names.length; i++) {
+  new Product(Product.names[i]);
 }
 
-  function displayPics() {
-    var currentlyShowing = [];
-    //make the left image unique
+function makeRandom() {
+  return Math.floor(Math.random() * Product.names.length);
+}
+
+function displayPics() {
+  var currentlyShowing = [];
+  //make left image unique
+  currentlyShowing[0] = makeRandom();
+  while (Product.justViewed.indexOf(currentlyShowing[0]) !== -1) {
+    console.error('Duplicate, rerun!');
     currentlyShowing[0] = makeRandom();
-    while (Product.justViewed.indexOf(currentlyShowing[0]) !== -1) {
-      console.log('Duplicate, rerun!');
-      currentlyShowing[0] = makeRandom();
-    }
-    //make the center image unique
+  }
+  //make center image unique
+  currentlyShowing[1] = makeRandom();
+  while (currentlyShowing[0] === currentlyShowing[1] || Product.justViewed.indexOf(currentlyShowing[1]) !== -1) {
+    console.error('Duplicate at center or in prior view! Re run!');
     currentlyShowing[1] = makeRandom();
-    while (currentlyShowing[0] === currentlyShowing[1] || Product.justViewed.indexOf(currentlyShowing[1]) !== -1) {
-      console.error('Duplicate at center or in prior view! Re run!');
-      currentlyShowing[1] = makeRandom();
-    }
-    //make the right image unique
+  }
+  //make right image unique
+  currentlyShowing[2] = makeRandom();
+  while (currentlyShowing[0] === currentlyShowing[2] || currentlyShowing[1] === currentlyShowing[2] || Product.justViewed.indexOf(currentlyShowing[2]) !== -1) {
+    console.error('Duplicate at right! re run it.');
     currentlyShowing[2] = makeRandom();
-    while (currentlyShowing[0] === currentlyShowing[2] || currentlyShowing[1] === currentlyShowing[2] || Product.justViewed.indexof(currentlyShowing[2]) !== -1) {
-      console.error('Duplicate at right! Re run it.');
-      currentlyShowing[2] = makeRandom();
-    }
-    
-    displayPics();
-
-    for (var i = 0; i < 3; i++) {
-      product.pics[i].src = Product.all[currentlyShowing [i]].path;
-      product.pics[i].id = Product.all [currentlyShowing[i]].name;
-      Product.all[currentlyShowing[i]].views += 1;
-      Product.justViewed[i] = currentlyShowing[i];
-
-    } 
-    
-    // event listener for keeping track of total clicks on images
-    function handleClick(event){
-console.log (Product.totalClicks, 'total clicks');
-if (Product.totalClicks > 24) {
-  product.container.removeEventListener('click', handleClick);
-  showTally ();
-}
-if(event.target.id === 'image_container'){
-  return alert('need to click on an image.')
-}
-Product.totalClicks += 1;
-for(var i = 0; i < Product.names.length; i++){
-  if(event.targer.id === Product.all[i].name)
-  Product.all[i].votes += 1;
-  console.log(event.target.id + ' has ' + Product.all[i].votes + 'votes in ' + Product.all[i].views + ' views');
-}
-    }
-
-    displayPics();
   }
-
-  function showTally(){
-    for (var i = 0; i < Product.all.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = Product.all[i].name + ' has ' + Product.all[i].votes + ' votes in'+ Product.all[i].views + ' views.';
-      Product.tally.appendChild(liEL);
+  //take it to the DOM
+  for (var i = 0; i < 3; i++) {
+    Product.pics[i].src = Product.all[currentlyShowing[i]].path;
+    Product.pics[i].id = Product.all[currentlyShowing[i]].name;
+    Product.all[currentlyShowing[i]].views += 1;
+    Product.justViewed[i] = currentlyShowing[i];
+  }
+}
+//event listener for keeping track of total clicks on images
+function handleClick(event) {
+  console.log(Product.totalClicks, 'total clicks');
+  //make the clicks stop at 25
+  if (Product.totalClicks > 24) {
+    Product.container.removeEventListener('click', handleClick);
+    //show the list after the last click
+    showTally();
+  }
+  //this is how we direct the user to click on a specific image
+  if (event.target.id === 'image_container') {
+    return alert('Need to click on an image.');
+  }
+  //start to add up the total clicks and log it to the console
+  Product.totalClicks += 1;
+  for (var i = 0; i < Product.names.length; i++) {
+    if (event.target.id === Product.all[i].name) {
+      Product.all[i].votes += 1;
+      console.log(event.target.id + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views.');
     }
   }
+  displayPics();
+}
 
-  Product.container.addEventListener('click', handleClick);
+//show the tally using the list in the DOM once the event listener has been removed
+function showTally() {
+  for (var i = 0; i < Product.all.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = Product.all[i].name + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views.';
+    //append the li to the Product.tally created above globally for the ul
+    Product.tally.appendChild(liEl);
+  }
+}
+//event listener
+Product.container.addEventListener('click', handleClick);
+displayPics();
+
+
+// create the chart////
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
